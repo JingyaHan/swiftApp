41cd98f4d7e94649dc239cc8940e7dde0427aefc
JINGYA HAN <big_big_wolf@dingdong.local>
2017-01-25 18:03:20 +1100

    Initial Commit

---
+++ SwiftAppOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftAppOne
+//
+//  Created by JINGYA HAN on 2017/1/25.
+//  Copyright © 2017年 JINGYA HAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
