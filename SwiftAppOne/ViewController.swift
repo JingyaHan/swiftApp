5ee86af794288f18b8b3ad474ef4f244b8365e3a
JINGYA HAN <hjy199411@163.com>
2017-01-31 13:49:33 +1100

    Boolean fun.

--- SwiftAppOne/ViewController.swift
+++ SwiftAppOne/ViewController.swift
@@ -11,13 +11,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var theLabel: UILabel!
     
     var tapCount = 0
     
     @IBAction func buttonTapped(_ sender: Any) {
-
+        
         theLabel.text = "Hello there!!!!!!!!!!!!"
         print("Nice to meet you")
         tapCount = tapCount + 1
@@ -35,7 +35,13 @@ class ViewController: UIViewController {
     
     @IBAction func add(_ sender: Any) {
         
-        theLabel.text = "You get: \(Double(text1.text!)! + Double(text2.text!)!)"
+        var addition = true
+        
+        if addition {            theLabel.text = "You get: \(Double(text1.text!)! + Double(text2.text!)!)"
+        } else{
+            theLabel.text = "You get: \(Double(text1.text!)! - Double(text2.text!)!)"
+        }
+        
         
     }
     @IBAction func LOL(_ sender: Any) {
@@ -51,12 +57,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
