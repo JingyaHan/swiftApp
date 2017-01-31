263cadaecb997710a0fcbc1c6d8637752dfedf43
JINGYA HAN <hjy199411@163.com>
2017-01-31 11:55:22 +1100

    My first commit.

--- SwiftAppOne/ViewController.swift
+++ SwiftAppOne/ViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+
+        theLabel.text = "Hello there!!!!!!!!!!!!"
+        print("Nice to meet you")
+        tapCount = tapCount + 1
+        print(tapCount)
+        if tapCount >= 10 {
+            print("dearest 黄彦云啊")
+            theLabel.text = "黄彦云啊黄彦云piu"
+        }
+        
+    }
+    
+    @IBAction func LOL(_ sender: Any) {
+        
+        theLabel.text = "And I like you :DDD"
+        print("yeah I reaaaaaally do")
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
